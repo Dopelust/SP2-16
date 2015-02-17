f39f624987fd43dbbcacaf5ca02de44ab56baf92
Dopelust <hibaricgg@hotmail.com>
2015-02-17 11:36:53 +0800

    FONT

--- MyGraphics/Source/PROJECTScene.cpp
+++ MyGraphics/Source/PROJECTScene.cpp
@@ -225,7 +225,7 @@ void PROJECTScene::Init()
 	meshList[GEO_CUBE] = MeshBuilder::GenerateCube("Cube", Color(1,1,1), 0.1f, 0.1f, 0.1f, 1);
 	meshList[GEO_BIGCUBE] = MeshBuilder::GenerateCube("Cube", Color(1,1,1), 36.f, 36.f, 36.f, 1);
 
-	meshList[GEO_TEXT] = MeshBuilder::GenerateText("text", 16, 16);
+	meshList[GEO_TEXT] = MeshBuilder::GenerateText("text", 16.f, 16.f);
 	meshList[GEO_TEXT]->textureID = LoadTGA("Image//font.tga");
 	
 	meshList[GEO_KNIFE] = MeshBuilder::GenerateOBJ("knife", "OBJ//arm2.obj");
@@ -623,7 +623,7 @@ void PROJECTScene::RenderText(Mesh* mesh, std::string text, Color color)
 	for(unsigned i = 0; i < text.length(); ++i)
 	{
 		Mtx44 characterSpacing;
-		characterSpacing.SetToTranslation(i * 1.0f, 0, 0); //1.0f is the spacing of each character, you may change this value
+		characterSpacing.SetToTranslation(i * 1.f, 0, 0); //1.0f is the spacing of each character, you may change this value
 		Mtx44 MVP = projectionStack.Top() * viewStack.Top() * modelStack.Top() * characterSpacing;
 		glUniformMatrix4fv(m_parameters[U_MVP], 1, GL_FALSE, &MVP.a[0]);
 	
