01931378930272e5e7bbfe7d4e55bbd0065c93dc
Dopelust <hibaricgg@hotmail.com>
2015-02-23 16:01:24 +0800

    npc framework update

--- MyGraphics/Source/NPC.cpp
+++ MyGraphics/Source/NPC.cpp
@@ -5,29 +5,72 @@ void NPC::Init()
 	Mesh* tempMesh;
 
 	tempMesh = MeshBuilder::GenerateOBJ("Head", "OBJ//CharOBJ//Head.obj");
-	bodyParts[HEAD] = Bodypart(position, Vector3(0,5.25f,0), tempMesh);
+	bodyParts[HEAD] = Bodypart(position, Vector3(0,5.25f,0), Vector3(1.5f, 1.5f, 1.5f), tempMesh);
 
 	tempMesh = MeshBuilder::GenerateOBJ("Body", "OBJ//CharOBJ//Body.obj");
-	bodyParts[BODY] = Bodypart(position, Vector3(0,3.375f,0), tempMesh);
+	bodyParts[BODY] = Bodypart(position, Vector3(0,3.375f,0), Vector3(1.5f,2.25f,0.75f), tempMesh);
 
 	tempMesh = MeshBuilder::GenerateOBJ("Left Arm", "OBJ//CharOBJ//LeftArm.obj");
-	bodyParts[L_ARM] = Bodypart(position, Vector3(-1.125f,3.375f,0), tempMesh);
+	bodyParts[L_ARM] = Bodypart(position, Vector3(-1.125f,3.375f,0), Vector3(0.75f, 2.25f, 0.75f), tempMesh);
 
 	tempMesh = MeshBuilder::GenerateOBJ("Right Arm", "OBJ//CharOBJ//RightArm.obj");
-	bodyParts[R_ARM] = Bodypart(position, Vector3(1.125f,3.375f,0), tempMesh);
+	bodyParts[R_ARM] = Bodypart(position, Vector3(1.125f,3.375f,0), Vector3(0.75f, 2.25f, 0.75f), tempMesh);
 
 	tempMesh = MeshBuilder::GenerateOBJ("Left Leg", "OBJ//CharOBJ//LeftLeg.obj");
-	bodyParts[L_LEG] = Bodypart(position, Vector3(-0.375f,1.125f,0), tempMesh);
+	bodyParts[L_LEG] = Bodypart(position, Vector3(-0.375f,1.125f,0), Vector3(0.75f, 2.25f, 0.75f), tempMesh);
 
 	tempMesh = MeshBuilder::GenerateOBJ("Right Leg", "OBJ//CharOBJ//RightLeg.obj");
-	bodyParts[R_LEG] = Bodypart(position, Vector3(0.375f,1.125f,0), tempMesh);
+	bodyParts[R_LEG] = Bodypart(position, Vector3(0.375f,1.125f,0), Vector3(0.75f, 2.25f, 0.75f), tempMesh);
 
 	for (int i = 0; i < NUM_BODYPARTS; i++)
 	{
 		bodyParts[i].ignoreCollision = true;
+		rotation[i] = 0.f;
+
+		if (i % 2 == 0)
+			rotationDir[i] = 1.f;
+		else
+			rotationDir[i] = -1.f;
 	}
 }
 
+void NPC::Animate(double dt, float speed)
+{
+	if (velocity.x == 0 && velocity.z == 0) //resetting to original location
+	{	
+		/*
+		if(value[bobbingX] * bobXDir >= 0) //need to minus to reset
+			bobXDir = -bobXDir;
+
+		else if(value[bobbingX] * bobXDir <= 0) //need to plus to reset
+			bobXDir = bobXDir;
+
+		if(!(value[bobbingX] > -0.05f && value[bobbingX] < 0.05f)) //if not 0, reset
+			value[bobbingX] += (float)(bobXDir * 1.0f * dt); 
+
+		if(value[bobbingY] * bobYDir >= 0) //need to minus to reset
+			bobYDir = -bobYDir;
+
+		else if(value[bobbingY] * bobYDir <= 0) //need to plus to reset
+			bobYDir = bobYDir;
+
+		if(!(value[bobbingY] > -0.05f && value[bobbingY] < 0.05f)) //if not 0, reset
+			value[bobbingY] += (float)(bobYDir * 1.0f * dt); 
+			*/
+	}
+
+	else
+	{
+		for (int i = L_ARM; i < NUM_BODYPARTS; i++)
+		{
+			if(rotation[i] * rotationDir[i] > 25.f)
+			{
+				rotationDir[i] = -rotationDir[i];
+			}
+			rotation[i] += (float)(rotationDir[i] * speed * dt);
+		}
+	}
+}
 void Hobo::Init()
 {
 	identity = "Homeless Man";
@@ -39,8 +82,6 @@ void Hobo::Init()
 		bodyParts[i].identity = identity;
 	}
 }
-
-float hoboHitDelay = 0.f;
 
 void Hobo::Update(double dt, vector<Object*>object, Player* player)
 {
@@ -78,19 +119,20 @@ void Hobo::Update(double dt, vector<Object*>object, Player* player)
 	position += velocity * (float)dt; 
 	RespondToCollision(initialPos, object, player);
 
+	//Animate(dt);
 	Material color;
-	if (hoboHitDelay > 0)
+	if (hitDelay > 0)
 	{
-		hoboHitDelay -= dt;
+		hitDelay -= dt;
 
-		if (hoboHitDelay > 0.2f)
+		if (hitDelay > 0.2f)
 		{
 			color.kAmbient.Set(0.92f,0.34f,0.29f);
 			color.kDiffuse.Set(0.92f,0.34f,0.29f);
 		}
 	}
 	else 
-		hoboHitDelay = 0;
+		hitDelay = 0;
 
 	for (int i = 0; i < NUM_BODYPARTS; i++)
 	{
@@ -102,7 +144,7 @@ void Hobo::Update(double dt, vector<Object*>object, Player* player)
 
 void Hobo::Control(double dt, vector<Object*>object, Player* player)
 {
-	if (object[player->camera.lookAt] == this && Application::mouseButton(0) && hoboHitDelay == 0)
+	if (object[player->camera.lookAt] == this && Application::mouseButton(0) && hitDelay == 0)
 	{
 		Vector3 direction;
 		direction.SphericalToCartesian(player->hOrientation, 0.f);
@@ -110,9 +152,9 @@ void Hobo::Control(double dt, vector<Object*>object, Player* player)
 		velocity.x += direction.x * 25;
 		velocity.z += direction.z * 25;
 		velocity.y += 15;
-		hoboHitDelay = 0.5f;
+		hitDelay = 0.5f;
 	}
-	else if (hoboHitDelay == 0)
+	else if (hitDelay == 0)
 	{
 		Vector3 direction;
 		direction.SphericalToCartesian(orientation, 0.f);
@@ -134,7 +176,6 @@ void Thug::Init()
 	}
 }
 
-float thugHitDelay = 0;
 void Thug::Update(double dt, vector<Object*>object, Player* player)
 {
 	Vector3 initialPos = position;
@@ -171,19 +212,21 @@ void Thug::Update(double dt, vector<Object*>object, Player* player)
 	position += velocity * (float)dt; 
 	RespondToCollision(initialPos, object, player);
 
-	Material color;
-	if (thugHitDelay > 0)
-	{
-		thugHitDelay -= dt;
+	Animate(dt, 80.f);
 
-		if (thugHitDelay > 0.2f)
+	Material color;
+	if (hitDelay > 0)
+	{
+		hitDelay -= dt;
+
+		if (hitDelay > 0.2f)
 		{
 			color.kAmbient.Set(0.92f,0.34f,0.29f);
 			color.kDiffuse.Set(0.92f,0.34f,0.29f);
 		}
 	}
 	else 
-		thugHitDelay = 0;
+		hitDelay = 0;
 
 	for (int i = 0; i < NUM_BODYPARTS; i++)
 	{
@@ -219,7 +262,7 @@ void Thug::Control(double dt, vector<Object*>object, Player* player)
 		}
 	}
 
-	if (object[player->camera.lookAt] == this && Application::mouseButton(0) && thugHitDelay == 0)
+	if (object[player->camera.lookAt] == this && Application::mouseButton(0) && hitDelay == 0)
 	{
 		Vector3 direction;
 		direction.SphericalToCartesian(player->hOrientation, 0.f);
@@ -227,9 +270,9 @@ void Thug::Control(double dt, vector<Object*>object, Player* player)
 		velocity.x += direction.x * 25;
 		velocity.z += direction.z * 25;
 		velocity.y += 15;
-		thugHitDelay = 0.5f;
+		hitDelay = 0.5f;
 	}
-	else if (thugHitDelay == 0)
+	else if (hitDelay == 0)
 	{
 		direction.SphericalToCartesian(orientation, 0.f);
 		velocity.x = direction.x * 4.f;
