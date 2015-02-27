b1fe2576b7477551f2800d326bd76eea57979b35
Dopelust <hibaricgg@hotmail.com>
2015-02-27 10:34:28 +0800

    My version

--- MyGraphics/Source/dialogue.cpp
+++ MyGraphics/Source/dialogue.cpp
@@ -5,127 +5,12 @@
 
 Vector3 TextBox::position = Vector3(0,-7.75f,0);
 
-void Detective::InitDialogue()
+void NPC::InitDialogue(const char* filename)
 {
 	ifstream inData;
 	string data;
 
-	inData.open ("Filestream//detective.txt"); 
-
-	while (!inData.eof()) {
-
-		getline (inData, data);
-
-		if (data == "")
-			continue;
-
-		greetings.push_back(TextBox(data, bodyParts[HEAD].mesh));
-	}
-
-	inData.close (); 
-}
-void Cashier::InitDialogue()
-{
-	ifstream inData;
-	string data;
-
-	inData.open ("Filestream//cashier.txt"); 
-
-	while (!inData.eof()) {
-
-		getline (inData, data);
-
-		if (data == "")
-			continue;
-
-		greetings.push_back(TextBox(data, bodyParts[HEAD].mesh));
-	}
-
-	inData.close (); 
-}
-void Hobo::InitDialogue()
-{
-	ifstream inData;
-	string data;
-
-	inData.open ("Filestream//hobo.txt"); 
-
-	while (!inData.eof()) {
-
-		getline (inData, data);
-
-		if (data == "")
-			continue;
-
-		greetings.push_back(TextBox(data, bodyParts[HEAD].mesh));
-	}
-
-	inData.close (); 
-}
-void Thug::InitDialogue()
-{
-	ifstream inData;
-	string data;
-
-	inData.open ("Filestream//thug.txt"); 
-
-	while (!inData.eof()) {
-
-		getline (inData, data);
-
-		if (data == "")
-			continue;
-
-		greetings.push_back(TextBox(data, bodyParts[HEAD].mesh));
-	}
-
-	inData.close (); 
-}
-void Blindman::InitDialogue()
-{
-	ifstream inData;
-	string data;
-
-	inData.open ("Filestream//blindman.txt"); 
-
-	while (!inData.eof()) {
-
-		getline (inData, data);
-
-		if (data == "")
-			continue;
-
-		greetings.push_back(TextBox(data, bodyParts[HEAD].mesh));
-	}
-
-	inData.close (); 
-}
-void Customer::InitDialogue()
-{
-	ifstream inData;
-	string data;
-
-	inData.open ("Filestream//customer.txt"); 
-
-	while (!inData.eof()) {
-
-		getline (inData, data);
-
-		if (data == "")
-			continue;
-
-		greetings.push_back(TextBox(data, bodyParts[HEAD].mesh));
-	}
-
-	inData.close (); 
-}
-
-void S_Guard::InitDialogue()
-{
-	ifstream inData;
-	string data;
-
-	inData.open ("Filestream//security.txt"); 
+	inData.open (filename); 
 
 	while (!inData.eof()) {
 
